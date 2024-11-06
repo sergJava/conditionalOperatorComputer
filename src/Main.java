0ba1dd2595ddcc4a5e01196e2cee6a91e4f5f748
sergFibih <sergfibih@ya.ru>
2024-11-06 16:24:12 +1000

    урок 2

--- src/Main.java
+++ src/Main.java
@@ -4,43 +4,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
         int clientOS = 1;
-        if (clientOS == 0){
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для IOS по ссылке.");
-        } else if (clientOS == 1){
+        }
+        if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
-        } else {
-            System.out.println("некорректная операционная система");
         }
 
 
         System.out.println("Task 2");
         int clientDeviceYear = 2015;
-        if(clientDeviceYear < 2015){
-            if (clientOS == 0){
-                System.out.println("Установите облегченную версию приложения для IOS по ссылке.");
-            } else if (clientOS == 1){
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("некорректная операционная система");
-            }
-        } else {                                       //если clientDeviceYear >=2015
-            if (clientOS == 0){
-                System.out.println("Установите версию приложения для IOS по ссылке.");
-            } else if (clientOS == 1){
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("некорректная операционная система");
-            }
+
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке.");
+        }
+        if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+        if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке.");
+        }
+        if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
         }
 
 
         System.out.println("Task 3");
         int year = 1700;
-        if(year >= 1584){
-            if((year%4 == 0 && year%100 !=0) || year%400 == 0){
-                System.out.println(year +" год является високосным.");
+        if (year >= 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println(year + " год является високосным.");
             } else {
-                System.out.println(year +" год не является високосным.");
+                System.out.println(year + " год не является високосным.");
             }
         } else {
             System.out.println("год должен быть больше, чем 1584");
@@ -50,13 +45,13 @@ public class Main {
         System.out.println("Task 4");
         int deliveryDistance = 95;
         int deliveryTime = 1;
-        if (deliveryDistance <20){
-            System.out.println("потребуется дней: " +deliveryTime);
-        } else if (deliveryDistance >=20 && deliveryDistance <60){
-            System.out.println("потребуется дней: " + (deliveryTime+1));
-        } else if (deliveryDistance >=60 && deliveryDistance <=100) {
-            System.out.println("потребуется дней: " + (deliveryTime+2));
-        } else  {
+        if (deliveryDistance < 20) {
+            System.out.println("потребуется дней: " + deliveryTime);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("потребуется дней: " + (deliveryTime + 1));
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("потребуется дней: " + (deliveryTime + 2));
+        } else {
             System.out.println("доставки нет");
         }
 
@@ -64,7 +59,7 @@ public class Main {
         System.out.println("Task 5");
 
         int monthNumber = 12;
-        if (monthNumber <=12) {
+        if (monthNumber <= 12) {
             switch (monthNumber) {
                 case 12:
                 case 1:
